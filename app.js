7cd68f89fbad1290b8d7d0104bd55b11dae537b8
Yuri Anisimov <yurianisimov27@gmail.com>
2021-05-18 23:49:37 +0300

    fix: added import in model

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser')
-// const db = require('./db');
-// const user = require('./controllers/usercontroller');
-// const game = require('./controllers/gamecontroller');
+const db = require('./db');
+// const userController = require('./controllers/usercontroller');
+// const gameController = require('./controllers/gamecontroller');
 // const validateSession = require('./middleware/validate-session')
 
 const port = process.env.PORT || 4000;
@@ -11,9 +11,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 // app.use(validateSession);
 
-// db.sync();
-// app.use('/api/auth', user);
-// app.use('/api/game', game);
+db.authenticate()
+  .then(() => console.log('Checking database connection...'))
+  .catch((error) => console.log('Error' + error));
+
+// app.use('/api/auth', userController);
+// app.use('/api/game', gameController);
 
 app.listen(port, () => {
   console.log(`App is listening on port: ${port}`);
--- db.js
+++ db.js
@@ -1,18 +1,16 @@
 const {Sequelize} = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
+module.exports = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres'
-})
+});
 
-async function connect() {
-  console.log('Checking database connection...');
-  try {
-    await sequelize.authenticate();
-  } catch (error) {
-    process.exit(1);
-  }
-}
-
-module.exports = connect;
+// async function connect() {
+//   console.log('Checking database connection...');
+//   try {
+//     await sequelize.authenticate();
+//   } catch (error) {
+//     process.exit(1);
+//   }
+// }
