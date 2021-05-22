9aa0f598fb10b7ca7bbaf75611ccbabdeaa8a76a
Yuri Anisimov <yurianisimov27@gmail.com>
2021-05-22 15:44:11 +0300

    fix: warning about sequelize deprecated String

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const db = require('./db');
 // const userController = require('./controllers/usercontroller');
-// const gameController = require('./controllers/gamecontroller');
+const gameController = require('./controllers/gamecontroller');
 // const validateSession = require('./middleware/validate-session')
 
 const port = process.env.PORT || 4000;
@@ -16,7 +16,7 @@ db.authenticate()
   .catch((error) => console.log('Error' + error));
 
 // app.use('/api/auth', userController());
-// app.use('/api/game', gameController());
+app.use('/api/game', gameController);
 
 app.listen(port, () => {
   console.log(`App is listening on port: ${port}`);
